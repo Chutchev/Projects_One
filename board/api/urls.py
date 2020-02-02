5540a0ee0ee473a4e985ac38dfeafc5f3148fc2c
zagoruykov <vitalik.zgrk@yandex.ru>
2020-02-02 12:22:11 +0500

    Добавлен скелет фронта

--- board/api/urls.py
+++ board/api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, re_path
 from .views import PostListView,RetrievePostView
 
 urlpatterns = [
-     path('auth/', obtain_auth_token, name='auth'),
-     path('posts/', PostListView.as_view(), name='post_list'),
-     path('post/<int:pk>', RetrievePostView.as_view(), name='post_detail'),
+    # TODO: Вынести авторизацию в core модуль
+    path('auth/', obtain_auth_token, name='auth'),
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>', RetrievePostView.as_view(), name='post_detail'),
 ]
